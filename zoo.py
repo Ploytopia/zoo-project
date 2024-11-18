17e509ffe50316dd3baf29a8b4716a80355daf75
Tamonwan Tabloi <137302777+Ploytopia@users.noreply.github.com>
2024-11-18 09:02:59 +0000

    Add test and Edit showing tikets

--- zoo.py
+++ zoo.py
@@ -1,10 +1,21 @@
 class Zoo:
     def get_ticket_price(self, age):
-        if 0 < age <= 12:
+        if age < 0:
+            return "Invalid input"
+        elif 0 <= age <= 12:
             return 50
-        elif 13 <= age < 20:
+        elif 13 <= age <= 20:
             return 100
-        elif 21 < age <= 60:
+        elif 21 <= age <= 60:
             return 150
-        elif age >= 60:
+        elif age > 60:
             return 100
+
+if __name__ == "__main__" :
+    print("This is your ticket price:")
+    ages = [-1, 12, 13, 21, 70]   
+    zoo = Zoo()
+    
+    for age in ages:
+        price = zoo.get_ticket_price(age)
+        print(f"For age {age}, the ticket price is: {price}")
